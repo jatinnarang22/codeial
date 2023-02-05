1ce4b7d152662fec4d85f949f0c794d30a330916
jatin narang <jatin1273@chitkara.edu.in>
2023-02-06 00:32:13 +0530

    add auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,24 @@
 const User = require('../models/user')
+/** @type {import("express").RequestHandler} */
 module.exports.profile = function(req,res)
 {
-    return res.render('user_profile',{
-        title:"profile"
-       })
+    console.log(req.cookies);
+    if(req.cookies && req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:"User profile",
+                    user:user
+                   })
+            }
+            return res.redirect('/users/si gn-in');
+        });
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+    
+
 }
 module.exports.signup = function(req,res){
     return res.render("user_sign_up",{
@@ -11,6 +26,7 @@ module.exports.signup = function(req,res){
     })
 }
 module.exports.signin = function(req,res){
+    console.log(true);
     return res.render("user_sign_in",{
         title: "Codial | Sign Up"
     })
@@ -37,6 +53,26 @@ module.exports.create = function(req,res){
 }
 
 //sign in and create a session for user
-module.exports.createSession= function(req,res){
-    //Todo later
+module.exports.createSession = function(req,res){
+
+        //step to authenticate
+        // find the user
+        User.findOne({email : req.body.email},function(err,user){
+            if(err){console.log('error in finding user in signing in'); return;}
+              // handle user found
+                if(user){
+                    // handle passowrd which don't match
+                    if(user.password != req.body.password){
+                        return res.redirect('back');
+                    }
+                    // handle session creation 
+                    res.cookie('user_id',user.id);
+                    return res.redirect('/users/profile')
+              }
+              else{
+                console.log('user.found;');
+                // handle user not found
+                return res.redirect('back');
+              }
+        });
 }
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,6 @@ const UserController =require('../controllers/users_controller');
 router.get('/profile',UserController.profile);
 router.get("/sign-up",UserController.signup);
 router.get("/sign-in",UserController.signin);
-
 router.post('/create',UserController.create);
+router.post('/create-session',UserController.createSession);
 module.exports =router;
